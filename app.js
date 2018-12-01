74e6268b924e4f6e2294337546cb141d92d91461
jlarson7 <joshua-j-larson@uiowa.edu>
2018-12-01 15:38:17 -0600

    Renamed files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs'); //require filesystem module
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+  fs.readFile(__dirname + '/public/index.ejs', function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
